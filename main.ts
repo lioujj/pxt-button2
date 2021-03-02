bd58c1f3cce0cb58fcfad38fcf00f340df168eb8
lioujj <34760803+lioujj@users.noreply.github.com>
2021-03-02 12:09:15 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-//% weight=100 color=#1d8045 icon="\uf0ad" block="Button2"
+//% weight=100 color=#048258 icon="\uf0ad" block="Button2"
 namespace button2 {
-    export enum pushType {
+    export enum pushType2 {
         //% block="pressed"
         down = PulseValue.Low,
         //% block="released"
         up = PulseValue.High
     }
 
-    //% blockId=onBtnChanged block="when button |%btnPin| is| %dir"
+    //% blockId=onBtn2Changed block="when button |%btnPin| is| %dir"
     //% weight=90
-    export function onBtnChanged(btnPin: DigitalPin, dir: pushType, handler: Action): void {
+    export function onBtn2Changed(btnPin: DigitalPin, dir: pushType2, handler: Action): void {
         //設定腳位上拉電阻
         pins.setPull(btnPin, PinPullMode.PullUp);
         //設定腳位接收到脈衝是High或是Low的時候，要執行什麼程式
